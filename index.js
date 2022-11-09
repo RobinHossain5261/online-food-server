03829dea24dd45e4f60f49fbb3c18416d5c6ba6b
Robin Hossain <robinhossain5261@gmail.com>
2022-11-09 07:14:05 +0600

    all product add

--- index.js
+++ index.js
@@ -10,11 +10,29 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nbqc6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try {
+        const productCollection = client.db('onlineFood').collection('products');
+
+
+
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
 
 
 app.get('/', (req, res) => {
