cd502c80f3c10de50d7711e784cba36e281af755
Robin Hossain <robinhossain5261@gmail.com>
2022-11-09 19:11:27 +0600

    dynamic id complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,8 +18,13 @@ async function run() {
     try {
         const productCollection = client.db('onlineFood').collection('products');
 
-
-
+        //homepage 3 product
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.limit(3).toArray();
+            res.send(products);
+        })
 
         app.get('/products', async (req, res) => {
             const query = {};
@@ -27,6 +32,14 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
+
     }
     finally {
 
